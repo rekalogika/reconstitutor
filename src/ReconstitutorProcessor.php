d5e9b9ed702a1fa1d611d2b3fdb066eb68b57931
Priyadi Iman Nurcahyo <1102197+priyadi@users.noreply.github.com>
2025-05-03 16:25:47 +0700

    feat: add debug logging (#38)
    * feat: add debug logging
    
    * fix

--- src/ReconstitutorProcessor.php
+++ src/ReconstitutorProcessor.php
@@ -13,52 +13,75 @@ declare(strict_types=1);
 
 namespace Rekalogika\Reconstitutor;
 
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerAwareTrait;
 use Rekalogika\Reconstitutor\Contract\ReconstitutorInterface;
 use Rekalogika\Reconstitutor\Contract\ReconstitutorResolverInterface;
 
-final class ReconstitutorProcessor
+final class ReconstitutorProcessor implements LoggerAwareInterface
 {
+    use LoggerAwareTrait;
+
     public function __construct(
         private readonly ReconstitutorResolverInterface $resolver,
         private readonly ReconstitutorContainer $container,
     ) {}
 
     /**
-     * @return iterable<array-key,ReconstitutorInterface<object>>
+     * @return iterable<array{string,ReconstitutorInterface<object>}>
      */
     private function getReconstitutors(object $object): iterable
     {
         $serviceIds = $this->resolver->getReconstitutors($object::class);
 
         foreach ($serviceIds as $serviceId) {
-            yield $this->container->get($serviceId);
+            yield [$serviceId, $this->container->get($serviceId)];
         }
+    }
+
+    private function log(
+        object $object,
+        string $serviceId,
+        string $method
+    ): void {
+        $this->logger?->debug(
+            'Calling {method} of reconstitutor {serviceId} on {object}',
+            [
+                'method' => $method,
+                'serviceId' => $serviceId,
+                'object' => $object::class,
+            ]
+        );
     }
 
     public function onCreate(object $object): void
     {
-        foreach ($this->getReconstitutors($object) as $reconstitutor) {
+        foreach ($this->getReconstitutors($object) as [$id, $reconstitutor]) {
+            $this->log($object, $id, 'onCreate');
             $reconstitutor->onCreate($object);
         }
     }
 
     public function onLoad(object $object): void
     {
-        foreach ($this->getReconstitutors($object) as $reconstitutor) {
+        foreach ($this->getReconstitutors($object) as [$id, $reconstitutor]) {
+            $this->log($object, $id, 'onLoad');
             $reconstitutor->onLoad($object);
         }
     }
 
     public function onSave(object $object): void
     {
-        foreach ($this->getReconstitutors($object) as $reconstitutor) {
+        foreach ($this->getReconstitutors($object) as [$id, $reconstitutor]) {
+            $this->log($object, $id, 'onSave');
             $reconstitutor->onSave($object);
         }
     }
 
     public function onRemove(object $object): void
     {
-        foreach ($this->getReconstitutors($object) as $reconstitutor) {
+        foreach ($this->getReconstitutors($object) as [$id, $reconstitutor]) {
+            $this->log($object, $id, 'onRemove');
             $reconstitutor->onRemove($object);
         }
     }
